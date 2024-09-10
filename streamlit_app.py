1fcea54b4524b35441b6a1c388c34aeb75f9da9a
nicoR-dev <nicholas.omroberto@gmail.com>
2024-09-09 18:40:13 -0700

    Update streamlit_app.py
    testing radio v0.02

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,10 @@ import streamlit as st
 st.title("🎈 My new app")
 price = st.radio(
     'Choose your price range:',
-    ['Low: 7,000-10,000', 'Medium: 10,000-20,000', 'High']
+    [':Low: 7,000-10,000', 'Medium: 10,000-20,000', 'High:']
     captions=[
         '7,000-10,000',
         '10,000-20,000',
         '20,000+',
     ],
 )
-
