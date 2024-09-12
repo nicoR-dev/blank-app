91615d7d4b6ab8594cfd26e4ac6a66158a5d618b
nicoR-dev <nicholas.omroberto@gmail.com>
2024-09-11 18:07:41 -0700

    Update streamlit_app.py
    testing sidebar v01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,25 @@
 import streamlit as st
 
+#price range & housing info
 st.title('Test')
-priceRange = st.radio(
-    "What is your price range",
-    ["High", "Medium", "Low"],
-    captions=[
-        "$30,000+",
-        "$10,000-$20,000",
-        "Below $10,000",
-    ],
-)
 
 housing = st.radio(
     "What type of housing would you like?",
     ["On-campus", "Off-campus", "Else"]
 )
+
+#sidebar to select different different settings.
+with st.sidebar: 
+    priceRange = st.radio(
+        "What is your price range",
+        ["High", "Medium", "Low"],
+        captions=[
+            "$30,000+",
+            "$10,000-$20,000",
+            "Below $10,000",
+        ],
+    )
+
 
 if housing == "Else": 
     housingElse = st.text_input("What are you planning?")
